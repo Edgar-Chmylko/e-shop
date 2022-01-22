3441b6f939a54d05fcfabc65ab587e77d46712a7
audrius <audrius.mickeviciuss@gmail.com>
2022-01-22 14:16:47 +0200

    1 first day admin panel

---
+++ src/app/product.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+// import { Http, Response, Headers, URLSearchParams, RequestOptions } from '@angular/http';
+// import { Observable } from 'rxjs';
+// import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/map';
+
+// import { Product, ResponseResult } from `./models`;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor() { }
+}
