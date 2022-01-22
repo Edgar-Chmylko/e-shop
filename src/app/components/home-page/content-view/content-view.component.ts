69643f4bfecfb2baf860dfd353f0124a52c0d535
EvC21 <echmylko@gmail.com>
2022-01-22 13:32:37 +0200

    modified content-view

--- src/app/components/home-page/content-view/content-view.component.ts
+++ src/app/components/home-page/content-view/content-view.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-content-view',
   templateUrl: './content-view.component.html',
@@ -8,6 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContentViewComponent implements OnInit {
   constructor() {}
-
+  productName = 'Product Name';
+  productImage = '/assets/logo.png';
+  productPrice = '4.20$';
+  productList = [
+    {
+      productName: this.productName,
+      productPrice: this.productPrice,
+      productImage: this.productImage,
+    },
+    {
+      productName: this.productName,
+      productPrice: this.productPrice,
+      productImage: this.productImage,
+    },
+    {
+      productName: this.productName,
+      productPrice: this.productPrice,
+      productImage: this.productImage,
+    },
+  ];
   ngOnInit(): void {}
 }
